b352cb2edc2e5ac55df8e1bf58ae3a03fd4a1581
sihuxx <sihu714@gmail.com>
2026-01-25 17:17:04 +0900

    인덱스 페이지 수정

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>도서관 메인</title>
     <link rel="stylesheet" href="./style/style.css">
 </head>
+
 <body>
     <?php require_once './header.php'; ?>
+    <main class="index-box">
+        <div class="index-content">
+            <div class="index-img">
+                <img src="./images/library.jpg" alt="도서관 이미지">
+            </div>
+            <div>
+                <h1>PHP library</h1>
+                <p>기능반 PHP 과제 도서관 페이지</p>
+            </div>
+        </div>
+    </main>
 </body>
+
 </html>
