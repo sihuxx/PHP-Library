b21548fe38530712996fccb4ddde2444d02cd817
sihuxx <sihu714@gmail.com>
2026-01-27 13:18:22 +0900

    책 대여 유저 캘린더 형식 완성/대여화면 틀 잡는 중

---
+++ profile.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>프로필</title>
+  <link rel="stylesheet" href="./style/style.css">
+</head>
+
+<body>
+  <?php
+  require_once './header.php';
+  $idx = $_GET["idx"];
+  $user = db::fetch("select * from user where idx = '$idx'");
+  $books = db::fetchAll("select b.*, u.*, b.idx as book_id from book b inner join user_book u on b.idx = u.book_idx and u.user_idx = '$user->idx'")
+  ?>
+
+  <main class="view-box">
+    <header>
+      <div>
+        <h1>유저 프로필</h1>
+        <p>다른 유저의 정보를 확인하세요</p>
+      </div>
+    </header>
+    <div class="profile">
+      <header>
+        <div>
+          <h1><?=$user->name?>님</h1>
+          <p>@<?=$user->id?></p>
+          <p>가입 한 이메일: <?=$user->email?></p>
+        </div>
+      </header>
+      <div class="profile-content">
+        <h3>대여한 책</h3>
+      </div>
+      <div class="books">
+        <?php foreach($books as $book) { ?>
+        <div class="book">
+           <?php if($book->period > 7) { ?>
+          <span class="book-type red-btn btn">연체됨</span>
+          <?php } else { ?>
+          <span class="book-type white-btn btn">대여 중</span>
+            <?php }?>
+        <div class="book-img">
+          <img src="<?=$book->img?>" alt="<?=$book->title?>">
+        </div>
+        <div class="book-content">
+          <h3 class="book-title"><?=$book->title?></h3>
+          <p class="book-des"><?=$book->des?></p>
+          <p class="book-period">반납 기한: <?=$book->period?>일 남음</p>
+        </div>
+        <form method="post" action="./bookReturn.php" class="book-btns">
+          <input type="hidden" name="book_idx" value="<?=$book->book_id?>">
+          <button class="btn">반납</button>
+        </form>
+      </div>
+      <?php } ?>
+      </div>
+    </div>
+  </main>
+</body>
+
+</html>
---
+++ rentalUserList.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>책 대여 유저 조회</title>
+  <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+  <?php 
+    require_once './header.php';
+  ?>
+  <main class="view-box">
+    <header>
+      <div>
+        <h1>책 대여 유저 조회</h1>
+        <p>조회할 방법을 선택하세요</p>
+      </div>
+    </header>
+    <div class="choose-box">
+      <div>
+        <i class="fa-2x "></i>
+        <h3>캘린더</h3>
+        <p>날짜 별 책 대여 유저를 조회하세요</p>
+        <a href="#">조회하기</a>
+      </div>
+    </div>
+  </main>
+</body>
+</html>
