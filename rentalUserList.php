66252f38f8491837974b8beb0b5b4e980ca6aa9a
sihuxx <sihu714@gmail.com>
2026-01-27 13:41:58 +0900

    책 대여 유저 조회 선택 페이지 구현햇음!

--- rentalUserList.php
+++ rentalUserList.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>책 대여 유저 조회</title>
   <link rel="stylesheet" href="./style/style.css">
+  <link rel="stylesheet" href="./fontawesome/css/font-awesome.css">
 </head>
+
 <body>
-  <?php 
-    require_once './header.php';
+  <?php
+  require_once './header.php';
   ?>
   <main class="view-box">
     <header>
@@ -19,12 +22,23 @@
     </header>
     <div class="choose-box">
       <div>
-        <i class="fa-2x "></i>
+        <div>
+        <i class="fa fa-calendar-o fa-2x"></i>
         <h3>캘린더</h3>
-        <p>날짜 별 책 대여 유저를 조회하세요</p>
-        <a href="#">조회하기</a>
+        <p>날짜 별 책 대여 유저를 <br> 조회하세요.</p>
+        </div>
+        <a href="./rentalUserCalendar.php" class="btn">조회하기</a>
+      </div>
+      <div>
+        <div>
+          <i class="fa fa-table fa-2x"></i>
+          <h3>표</h3>
+          <p>표 형식으로 책 대여 유저를 <br> 조회하세요.</p>
+        </div>
+        <a href="./rentalUserTable.php" class="btn">조회하기</a>
       </div>
     </div>
   </main>
 </body>
+
 </html>
