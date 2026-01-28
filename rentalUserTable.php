bbb52eaa65977647b065990c48917a1a16716ff2
sihuxx <sihu714@gmail.com>
2026-01-28 10:58:48 +0900

    반납/대여 책 정렬 구현

--- rentalUserTable.php
+++ rentalUserTable.php
@@ -23,12 +23,17 @@
         <p>표로 책 대여 유저를 조회하세요.</p>
       </div>
     </header>
+    <div class="btn-box">
+      <button onclick="sort('반납됨')" class="white-btn btn">반납됨</button>
+      <button onclick="sort('대여중')" class="white-btn btn">대여중</button>
+    </div>
     <table class="nomal-table">
       <thead>
         <th>상태</th>
         <th>닉네임</th>
         <th>아이디</th>
         <th>책 이름</th>
+        <th>대여일</th>
         <th>반납 기한</th>
         <th>관리</th>
       </thead>
@@ -48,20 +53,34 @@
   const tableBody = document.querySelector("table tbody");
   const limit = 5;
   const pageInfo = document.querySelector(".page-info");
+  const btns = document.querySelectorAll(".btn-box button");
 
   let pageIndex = 0; // 현재 페이지
   let data = [...userData];
+
+  btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      btns.forEach(e => e.classList.add("white-btn"));
+      btn.classList.remove("white-btn");
+    })
+  })
+
+  function sort(val) {
+    data = val == "대여중" ? userData.filter(n => n.is_rental == 1) : userData.filter(n => n.is_rental == 0);
+    renderUser();
+  }
 
   function renderUser() {
     tableBody.innerHTML = data.slice(pageIndex * limit, (pageIndex + 1) * limit).map(n =>
       `
       <tr>
         <td>
-          <span class="user-type" style="background-color: #9cdc12;">${n.is_rental == '1' ? "대여중" : "반납됨"}</span>
+          <span class="user-type" style="background-color: ${n.is_rental == '1' ? "#9cdc12" : "#ff7474"};">${n.is_rental == '1' ? "대여중" : "반납됨"}</span>
         </td>
         <td>${n.name}</td>
         <td>${n.id}</td>
         <td>${n.title}</td>
+        <td>${n.rental_date}</td>
         <td>${n.period}일</td>
         <td>
         <a href="profile.php?idx=${n.user_id}" class="btn white-btn">프로필 보기</a>
