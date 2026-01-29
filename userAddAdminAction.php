72b434a5ef7d575d155dd9efc0a611f8f01d4a39
sihuxx <sihu714@gmail.com>
2026-01-29 15:11:54 +0900

    서점 관리자 추가 alert 중복 오류

--- userAddAdminAction.php
+++ userAddAdminAction.php
@@ -8,10 +8,9 @@ extract($_POST);
 if(db::fetch("select * from stores where idx = '$store_idx' and admin_idx is null")) {
   db::exec("update stores set admin_idx = '$user_idx' where idx = '$store_idx'");
   db::exec("update user set admin = '1' where idx = '$user_idx'");
+  alert("서점 관리자가 추가되었습니다");
+  move("./userAdmin.php");
 } else {
   alert("이미 서점 관리자가 존재하는 서점입니다");
   move("./userAdmin.php");
 }
-
-alert("서점 관리자가 추가되었습니다");
-move("./userAdmin.php");
